9fdfa2949c0fe867d9fc99ceccc3ac029da40769
Asif Rana <rasifrana@hotmail.com>
2019-05-09 10:34:44 +0100

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,139 @@
-let order = [];
-let playerOrder = [];
-let flash;
-let turn;
-let good;
-let compTurn;
-let intervalId;
-let strict = false;
-let noise = true;
-let on = false;
-let win;
+var order = [];
+var playerOrder = [];
+var flash;
+var turn;
+var good;
+var compTurn;
+var intervalId;
+var strict = false;
+var noise = true;
+var on = false;
+var win;
 
-const turnCounter = document.querySelector("#turn");
-const topLeft = document.querySelector("#topleft");
-const topRight = document.querySelector("#topright");
-const bottomLeft = document.querySelector("#bottomleft");
-const bottomRight = document.querySelector("#bottomright");
-const strictButton = document.querySelector("#strict");
-const onButton = document.querySelector("#on");
-const startButton = document.querySelector("#start");
+// **************** All Selectors *************
+
+var turnCounter = document.querySelector("#turn");
+var topLeft = document.querySelector("#topleft");
+var topRight = document.querySelector("#topright");
+var bottomLeft = document.querySelector("#bottomleft");
+var bottomRight = document.querySelector("#bottomright");
+var strictButton = document.querySelector("#strict");
+var onButton = document.querySelector("#on");
+var startButton = document.querySelector("#start");
+
+// ************** Ristrict Mode ON/OFF *********
+
+strictButton.addEventListener("click", function(e) {
+  if (strictButton.checked == true) {
+    strict = true;
+  } else {
+    strict = false;
+  }
+});
+
+// ************ Game ON / OFF func **********
+onButton.addEventListener("click", function(e) {
+  if (onButton.checked == true) {
+    on = true;
+    turnCounter.innerHTML = "-";
+  } else {
+    on = false;
+    turnCounter.innerHTML = "";
+    clearColor();
+    clearInterval(intervalId);
+  }
+});
+
+// ********************** Game Start func ********
+// startButton.addEventListener("click", function(e) {
+//   if (on || win) {
+//     play();
+//   }
+// });
+
+// function play() {
+//   win = false;
+//   order = [];
+//   playerOrder = [];
+//   flash = 0;
+//   intervalId = 0;
+//   turn = 1;
+//   turnCounter.innerHTML = 1;
+//   good = true;
+//   for (var i = 0; i < 10; i++) {
+//     order.push(Math.ceil(Math.random() * 4));
+//   }
+//   compTurn = true;
+
+//   intervalId = setInterval(gameTurn, 800);
+// }
+
+// function gameTurn() {
+//   on = false;
+
+//   if (compTurn) {
+//     clearColor();
+//     setTimeout(() => {
+//       if (order[flash] == 1) one();
+//       if (order[flash] == 2) two();
+//       if (order[flash] == 3) three();
+//       if (order[flash] == 4) four();
+//       flash++;
+//     }, 200);
+//   }
+
+//   if (flash == turn) {
+//     clearInterval(intervalId);
+//     compTurn = false;
+//     clearColor();
+//     on = true;
+//   }  
+// }
+
+
+// function one() {
+//   if (noise) {
+//     let audio = document.getElementById("clip1");
+//     audio.play();
+//   }
+//   noise = true;
+//   topLeft.style.backgroundColor = "lightgreen";
+// }
+
+// function two() {
+//   if (noise) {
+//     let audio = document.getElementById("clip2");
+//     audio.play();
+//   }
+//   noise = true;
+//   topRight.style.backgroundColor = "tomato";
+// }
+
+// function three() {
+//   if (noise) {
+//     let audio = document.getElementById("clip3");
+//     audio.play();
+//   }
+//   noise = true;
+//   bottomLeft.style.backgroundColor = "yellow";
+// }
+
+// function four() {
+//   if (noise) {
+//     let audio = document.getElementById("clip4");
+//     audio.play();
+//   }
+//   noise = true;
+//   bottomRight.style.backgroundColor = "lightskyblue";
+// }
+
+// function clearColor() {
+//   topLeft.style.backgroundColor = "darkgreen";
+//   topRight.style.backgroundColor = "darkred";
+//   bottomLeft.style.backgroundColor = "goldenrod";
+//   bottomRight.style.backgroundColor = "darkblue";
+// }
+
 
 strictButton.addEventListener("click", event => {
   if (strictButton.checked == true) {
@@ -231,3 +347,5 @@ function winGame() {
   on = false;
   win = true;
 }
+
+
